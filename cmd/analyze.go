367ca3c5900d2adefeba90f539afd5085a8e59c1
Pranav Gaikwad <pgaikwad@redhat.com>
2024-01-15 14:00:42 -0500

    :ghost: make bundles location a config setting
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- cmd/analyze.go
+++ cmd/analyze.go
@@ -417,7 +417,7 @@ func (a *analyzeCommand) getConfigVolumes() (map[string]string, error) {
 				Location:     SourceMountPath,
 				AnalysisMode: provider.AnalysisMode(a.mode),
 				ProviderSpecificConfig: map[string]interface{}{
-					"bundles":                       "/jdtls/java-analyzer-bundle/java-analyzer-bundle.core/target/java-analyzer-bundle.core-1.0.0-SNAPSHOT.jar",
+					"bundles":                       JavaBundlesLocation,
 					"depOpenSourceLabelsFile":       "/usr/local/etc/maven.default.index",
 					provider.LspServerPathConfigKey: "/jdtls/bin/jdtls",
 				},
@@ -468,9 +468,9 @@ func (a *analyzeCommand) getConfigVolumes() (map[string]string, error) {
 	// Set proxy to providers
 	if a.httpProxy != "" || a.httpsProxy != "" {
 		proxy := provider.Proxy{
-			HTTPProxy: a.httpProxy,
+			HTTPProxy:  a.httpProxy,
 			HTTPSProxy: a.httpsProxy,
-			NoProxy: a.noProxy,
+			NoProxy:    a.noProxy,
 		}
 		for i := range provConfig {
 			provConfig[i].Proxy = &proxy
