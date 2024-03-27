78c50cac5e891a4e9f0107f830291d9df17b7e0e
Shawn Hurley <shawn@hurley.page>
2024-03-27 11:25:01 -0400

    :bug: fixing the location of the created programs to correct dir
    Signed-off-by: Shawn Hurley <shawn@hurley.page>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set -x
 
 cp -r /opt/input/source /tmp/source-code
 sed -i 's|/opt/input/source|/tmp/source-code|g' /opt/input/config/settings.json
-/usr/bin/konveyor-analyzer "$@" 
+/usr/local/bin/konveyor-analyzer "$@" 
 sed -i 's|/tmp/source-code|/opt/input/source|g' /opt/input/config/settings.json
