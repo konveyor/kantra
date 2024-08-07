f921c5148bae71faab14f6914b5482eafca478f0
soham4abc <sohambanerjee4abc@hotmail.com>
2024-08-07 19:45:39 +0530

    :bug :(env): changed default path for PODMAN_BIN
    Signed-off-by: soham4abc <sohambanerjee4abc@hotmail.com>

--- cmd/settings.go
+++ cmd/settings.go
@@ -25,7 +25,7 @@ const (
 type Config struct {
 	RootCommandName      string `env:"CMD_NAME" default:"kantra"`
 	ContainerBinary      string `env:"CONTAINER_TOOL" default:"/usr/bin/docker"`
-	PodmanBinary         string `env:"PODMAN_BIN" default:"/usr/bin/podman"`
+	PodmanBinary         string `env:"PODMAN_BIN" default:"/usr/bin/docker"`
 	RunnerImage          string `env:"RUNNER_IMG" default:"quay.io/konveyor/kantra"`
 	RunLocal             bool   `env:"RUN_LOCAL"`
 	JavaProviderImage    string `env:"JAVA_PROVIDER_IMG" default:"quay.io/konveyor/java-external-provider:latest"`
