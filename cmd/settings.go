18d9c1c9a412a5519a18d78b512159fed2425706
Dylan Murray <dymurray@redhat.com>
2026-01-07 16:11:14 -0500

    :bug: Change environment variable from DOTNET to CSHARP (#664)
    * Change environment variable from DOTNET to CSHARP
    
    Signed-off-by: Dylan Murray <dymurray@redhat.com>
    
    * Replace DotnetProviderImage with CsharpProviderImage
    
    Signed-off-by: Dylan Murray <dymurray@redhat.com>
    
    ---------
    
    Signed-off-by: Dylan Murray <dymurray@redhat.com>

--- cmd/settings.go
+++ cmd/settings.go
@@ -18,7 +18,7 @@ var (
 	RulesetsLocation     = "rulesets"
 	JavaProviderImage    = "quay.io/konveyor/java-external-provider"
 	GenericProviderImage = "quay.io/konveyor/generic-external-provider"
-	DotnetProviderImage  = "quay.io/konveyor/dotnet-external-provider"
+	CsharpProviderImage  = "quay.io/konveyor/c-sharp-provider"
 )
 
 var Settings = &Config{}
@@ -139,11 +139,11 @@ func (c *Config) loadProviders() error {
 		}
 	}
 
-	if os.Getenv("DOTNET_PROVIDER_IMG") == "" {
+	if os.Getenv("CSHARP_PROVIDER_IMG") == "" {
 		// if version tag is given in image
-		dotnetImg := strings.TrimSuffix(DotnetProviderImage, fmt.Sprintf(":%v", Version))
-		updatedDotnetImg := fmt.Sprintf("%v:%v", dotnetImg, Version)
-		err := os.Setenv("DOTNET_PROVIDER_IMG", updatedDotnetImg)
+		csharpImg := strings.TrimSuffix(CsharpProviderImage, fmt.Sprintf(":%v", Version))
+		updatedCsharpImg := fmt.Sprintf("%v:%v", csharpImg, Version)
+		err := os.Setenv("CSHARP_PROVIDER_IMG", updatedCsharpImg)
 		if err != nil {
 			return err
 		}
